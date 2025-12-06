31d70f76774f7d835d7979044b90700a12945a24
“Asfer <asfer1@hotmail.com>
2025-12-06 16:04:22 +0000

    Added New Solution

--- src/Leetcode/Group0001_1000/Solution0007_reverse_integer/Solution.php
+++ src/Leetcode/Group0001_1000/Solution0007_reverse_integer/Solution.php
@@ -8,4 +8,13 @@ namespace Leetcode\Group0001_1000\Solution0007_reverse_integer;
  */
 class Solution
 {
+    /**
+     * @param Integer $x
+     * @return Integer
+     */
+    function reverse($x)
+    {
+        $y = ($x < 0) ? -(int)strrev((string)$x) : (int)strrev((string)$x);
+        return ($y < -2147483648 || $y > 2147483647) ? 0 : $y;
+    }
 }
