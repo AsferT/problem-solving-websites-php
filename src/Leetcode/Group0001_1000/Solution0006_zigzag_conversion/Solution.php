0dd370bc2c8e15b3d106bdb229592476dc30b13c
“Asfer <asfer1@hotmail.com>
2025-12-06 15:52:53 +0000

    Added New Solution

--- src/Leetcode/Group0001_1000/Solution0006_zigzag_conversion/Solution.php
+++ src/Leetcode/Group0001_1000/Solution0006_zigzag_conversion/Solution.php
@@ -8,4 +8,43 @@ namespace Leetcode\Group0001_1000\Solution0006_zigzag_conversion;
  */
 class Solution
 {
+
+    /**
+     * @param String $s
+     * @param Integer $numRows
+     * @return String
+     */
+    function convert(string $s, int $numRows): string
+    {
+        $stringLen = strlen($s);
+
+        if (1 === $numRows || $stringLen <= $numRows) {
+            return $s;
+        }
+
+        $row = 0;
+        $stringArr = [];
+        $upDir = false;
+        $numRowsIndex = $numRows - 1;
+
+        for ($i = 0; $i < $stringLen; $i++) {
+            // Store Letter Position
+            $stringArr[$row] .= $s[$i];
+
+            // Row and Col Control
+            if (!$upDir && $row < $numRowsIndex) {
+                $row++;
+            } elseif ($upDir && $row > 0) {
+                $row--;
+            } elseif (!$upDir && $row === $numRowsIndex) {
+                $upDir = true;
+                $row--;
+            } elseif ($upDir && $row === 0) {
+                $upDir = false;
+                $row++;
+            }
+        }
+
+        return implode('', $stringArr);
+    }
 }
