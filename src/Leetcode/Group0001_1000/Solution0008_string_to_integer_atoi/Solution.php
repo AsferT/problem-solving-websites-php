c18fbf0f0b492cfa2451fb6bd31b174962fd6696
“Asfer <asfer1@hotmail.com>
2025-12-06 16:27:01 +0000

    Added New Solution

--- src/Leetcode/Group0001_1000/Solution0008_string_to_integer_atoi/Solution.php
+++ src/Leetcode/Group0001_1000/Solution0008_string_to_integer_atoi/Solution.php
@@ -8,4 +8,27 @@ namespace Leetcode\Group0001_1000\Solution0008_string_to_integer_atoi;
  */
 class Solution
 {
+    /**
+     * @param String $s
+     * @return Integer
+     */
+    function myAtoi(string $s): int
+    {
+        $s = ltrim($s);
+        $l = strlen($s);
+        for ($i = 0; $i < $l; $i++) {
+            if (($s[$i] >= 'A' && $s[$i] <= 'Z') || ($s[$i] >= 'a' && $s[$i] <= 'z')) {
+                $s = substr($s, 0, $i);
+                break;
+            }
+        }
+        $s = (int)$s;
+        if ($s >= 2147483647) {
+            return 2147483647;
+        }
+        if ($s <= -2147483648) {
+            return -2147483648;
+        }
+        return $s;
+    }
 }
