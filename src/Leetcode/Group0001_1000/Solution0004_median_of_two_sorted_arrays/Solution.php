8b63e61784799dbf9adf3b02d1e5fb7b27652853
“Asfer <asfer1@hotmail.com>
2025-12-06 14:21:30 +0000

    Added New Solution

--- src/Leetcode/Group0001_1000/Solution0004_median_of_two_sorted_arrays/Solution.php
+++ src/Leetcode/Group0001_1000/Solution0004_median_of_two_sorted_arrays/Solution.php
@@ -8,4 +8,24 @@ namespace Leetcode\Group0001_1000\Solution0004_median_of_two_sorted_arrays;
  */
 class Solution
 {
+    /**
+     * @param Integer[] $nums1
+     * @param Integer[] $nums2
+     * @return Float
+     */
+    function findMedianSortedArrays($nums1, $nums2)
+    {
+        $numsTotal = array_merge($nums1, $nums2);
+        sort($numsTotal);
+        $count = count($numsTotal);
+
+        if ($count % 2 == 0) {
+            $index = ($count/2);
+            $median = ($numsTotal[$index] + $numsTotal[$index-1])/2;
+        } else {
+            $index = (($count+1)/2)-1;
+            $median = $numsTotal[$index];
+        }
+        return $median;
+    }
 }
