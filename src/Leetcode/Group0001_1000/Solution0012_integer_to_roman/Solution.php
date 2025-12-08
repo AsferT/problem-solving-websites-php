be7d643096fb1d26b8084b36ec5b1f75dabf00b8
“Asfer <asfer1@hotmail.com>
2025-12-08 01:08:11 +0000

    Added New Solution

--- src/Leetcode/Group0001_1000/Solution0012_integer_to_roman/Solution.php
+++ src/Leetcode/Group0001_1000/Solution0012_integer_to_roman/Solution.php
@@ -8,4 +8,37 @@ namespace Leetcode\Group0001_1000\Solution0012_integer_to_roman;
  */
 class Solution
 {
+    /**
+     * @param Integer $num
+     * @return String
+     */
+    function intToRoman(int $num): string
+    {
+        $map = [
+            1000 => 'M',
+            900  => 'CM',
+            500  => 'D',
+            400  => 'CD',
+            100  => 'C',
+            90   => 'XC',
+            50   => 'L',
+            40   => 'XL',
+            10   => 'X',
+            9    => 'IX',
+            5    => 'V',
+            4    => 'IV',
+            1    => 'I',
+        ];
+
+        $result = '';
+
+        foreach ($map as $value => $roman) {
+            while ($num >= $value) {
+                $result .= $roman;
+                $num -= $value;
+            }
+        }
+
+        return $result;
+    }
 }
