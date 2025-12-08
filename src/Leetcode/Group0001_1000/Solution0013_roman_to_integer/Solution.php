40fe82df2093e9f1cfc1dde9ae6f02cc321f1d79
“Asfer <asfer1@hotmail.com>
2025-12-08 01:36:37 +0000

    Added New Solution

--- src/Leetcode/Group0001_1000/Solution0013_roman_to_integer/Solution.php
+++ src/Leetcode/Group0001_1000/Solution0013_roman_to_integer/Solution.php
@@ -8,4 +8,35 @@ namespace Leetcode\Group0001_1000\Solution0013_roman_to_integer;
  */
 class Solution
 {
+    /**
+     * @param String $s
+     * @return Integer
+     */
+    function romanToInt(string $s): int
+    {
+        $map = [
+            'M'  => 1000,
+            'CM' => 900,
+            'D'  => 500,
+            'CD' => 400,
+            'C'  => 100,
+            'XC' => 90,
+            'L'  => 50,
+            'XL' => 40,
+            'X'  => 10,
+            'IX' => 9,
+            'V'  => 5,
+            'IV' => 4,
+            'I'  => 1,
+        ];
+        $result = 0;
+        foreach ($map as $roman => $value) {
+            $romanLen = strlen($roman);
+            while (str_starts_with($s, $roman)) {
+                $result += $value;
+                $s = substr($s, $romanLen);
+            }
+        }
+        return $result;
+    }
 }
