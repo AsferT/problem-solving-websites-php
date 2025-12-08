a390ccac4fc0f03d95ac4a1f0f97e65480bec8c6
“Asfer <asfer1@hotmail.com>
2025-12-08 02:49:23 +0000

    Added New Solution. Too much debugging to get the solution correct

--- src/Leetcode/Group0001_1000/Solution0015_3sum/Solution.php
+++ src/Leetcode/Group0001_1000/Solution0015_3sum/Solution.php
@@ -8,4 +8,37 @@ namespace Leetcode\Group0001_1000\Solution0015_3sum;
  */
 class Solution
 {
+    /**
+     * @param Integer[] $nums
+     * @return Integer[][]
+     */
+    function threeSum(array $nums): array
+    {
+        sort($nums);
+        $count = count($nums);
+        $result = [];
+        for ($i = 0; $i < $count; $i++) {
+            if ($i > 0 && $nums[$i] === $nums[$i - 1]) {
+                continue;
+            }
+            $left = $i + 1;
+            $right = $count - 1;
+            while ($left < $right) {
+                $total = $nums[$i] + $nums[$left] + $nums[$right];
+                if ($total > 0) {
+                    $right--;
+                } elseif ($total < 0) {
+                    $left++;
+                } else {
+                    $result[] = [$nums[$i], $nums[$left], $nums[$right]];
+                    $left++;
+                    while (isset($nums[$left], $nums[$left-1]) && $nums[$left] === $nums[$left - 1]) {
+                        $left++;
+                    }
+                }
+            }
+        }
+
+        return $result;
+    }
 }
