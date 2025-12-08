cbd89ffeda36abbc70dfc0756ef38f94c41ade35
“Asfer <asfer1@hotmail.com>
2025-12-08 01:53:30 +0000

    Added New Solution

--- src/Leetcode/Group0001_1000/Solution0014_longest_common_prefix/Solution.php
+++ src/Leetcode/Group0001_1000/Solution0014_longest_common_prefix/Solution.php
@@ -8,4 +8,28 @@ namespace Leetcode\Group0001_1000\Solution0014_longest_common_prefix;
  */
 class Solution
 {
+    /**
+     * @param String[] $strs
+     * @return String
+     */
+    function longestCommonPrefix(array $strs): string
+    {
+        $initialString = $strs[0];
+        $initialStringLen = strlen($initialString);
+        $result = '';
+        for ($i = 0; $i < $initialStringLen; $i++) {
+            $stringCheck = $result . $initialString[$i];
+            $check = true;
+            foreach ($strs as $word) {
+                if (!str_starts_with($word, $stringCheck)) {
+                    $check = false;
+                }
+            }
+            if (!$check) {
+                return $result;
+            }
+            $result = $stringCheck;
+        }
+        return $result;
+    }
 }
