e6162b97c7c38bb904aebd8cd98097494eee8607
“Asfer <asfer1@hotmail.com>
2025-12-09 21:03:19 +0000

    Added another Solution

--- src/Leetcode/Group0001_1000/Solution0019_remove_nth_node_from_end_of_list/Solution.php
+++ src/Leetcode/Group0001_1000/Solution0019_remove_nth_node_from_end_of_list/Solution.php
@@ -6,6 +6,39 @@ namespace Leetcode\Group0001_1000\Solution0019_remove_nth_node_from_end_of_list;
  * Title: Remove Nth Node From End of List
  * Difficulty: medium
  */
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val = 0, $next = null) {
+ *         $this->val = $val;
+ *         $this->next = $next;
+ *     }
+ * }
+ */
 class Solution
 {
+    /**
+     * @param ListNode $head
+     * @param Integer $n
+     * @return ListNode
+     */
+    function removeNthFromEnd(ListNode $head, int $n): ?ListNode
+    {
+        $current = new ListNode(0);
+        $current->next = $head;
+        $dummy1 = $current;
+        $dummy2 = $current;
+        while ($dummy1 !== null) {
+            $dummy1 = $dummy1->next;
+            if ($n >= 0) {
+                $n--;
+            } else {
+                $dummy2 = $dummy2->next;
+            }
+        }
+        $dummy2->next = $dummy2->next?->next;
+        return $current->next;
+    }
 }
