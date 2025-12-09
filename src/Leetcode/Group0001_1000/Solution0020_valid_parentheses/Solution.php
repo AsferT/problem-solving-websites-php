e43e7c8b3fc432478a730a46763ec52c7dd7dca7
“Asfer <asfer1@hotmail.com>
2025-12-09 21:45:05 +0000

    Added another Solution

--- src/Leetcode/Group0001_1000/Solution0020_valid_parentheses/Solution.php
+++ src/Leetcode/Group0001_1000/Solution0020_valid_parentheses/Solution.php
@@ -8,4 +8,25 @@ namespace Leetcode\Group0001_1000\Solution0020_valid_parentheses;
  */
 class Solution
 {
+    /**
+     * @param String $s
+     * @return Boolean
+     */
+    function isValid(string $s): bool
+    {
+        $count = strlen($s);
+        if ($count % 2 !== 0) {
+            return false;
+        }
+        $stack = [];
+        $pairs = ['(' => ')', '[' => ']', '{' => '}'];
+        for ($i = 0; $i < $count; $i++) {
+            if (array_key_exists($s[$i], $pairs)) {
+                $stack[] = $pairs[$s[$i]];
+            } elseif (array_pop($stack) !== $s[$i]) {
+                return false;
+            }
+        }
+        return empty($stack);
+    }
 }
