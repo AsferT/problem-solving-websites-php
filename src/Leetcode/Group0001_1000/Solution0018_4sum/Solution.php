e1819a08b1512830a833e804e31f9e49b4aac0b9
“Asfer <asfer1@hotmail.com>
2025-12-09 20:35:51 +0000

    Added another Solution

--- src/Leetcode/Group0001_1000/Solution0018_4sum/Solution.php
+++ src/Leetcode/Group0001_1000/Solution0018_4sum/Solution.php
@@ -8,4 +8,46 @@ namespace Leetcode\Group0001_1000\Solution0018_4sum;
  */
 class Solution
 {
+    /**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer[][]
+     */
+    function fourSum(array $nums, int $target): array
+    {
+        sort($nums);
+        $count = count($nums);
+        $result = [];
+        for ($i = 0; $i < $count - 3; $i++) {
+            if ($i > 0 && $nums[$i] === $nums[$i - 1]) {
+                continue;
+            }
+            for ($j = $i + 1; $j < $count - 2; $j++) {
+                if ($j > $i + 1 && $nums[$j] === $nums[$j - 1]) {
+                    continue;
+                }
+                $left = $j + 1;
+                $right = $count - 1;
+                while ($left < $right) {
+                    $total = $nums[$i] + $nums[$j] + $nums[$left] + $nums[$right];
+                    if ($total === $target) {
+                        $result[] = [$nums[$i], $nums[$j], $nums[$left], $nums[$right]];
+                        while ($left < $right && $nums[$left] === $nums[$left + 1]) {
+                            $left++;
+                        }
+                        while ($left < $right && $nums[$right] === $nums[$right - 1]) {
+                            $right--;
+                        }
+                        $left++;
+                        $right--;
+                    } elseif ($total < $target) {
+                        $left++;
+                    } else {
+                        $right--;
+                    }
+                }
+            }
+        }
+        return $result;
+    }
 }
