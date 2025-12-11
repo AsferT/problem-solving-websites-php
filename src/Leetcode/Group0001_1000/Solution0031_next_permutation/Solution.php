bffb918241ff65420fee1156a45533450fdb6cd8
“Asfer <asfer1@hotmail.com>
2025-12-11 11:48:26 +0000

    Added another Solution

--- src/Leetcode/Group0001_1000/Solution0031_next_permutation/Solution.php
+++ src/Leetcode/Group0001_1000/Solution0031_next_permutation/Solution.php
@@ -8,4 +8,33 @@ namespace Leetcode\Group0001_1000\Solution0031_next_permutation;
  */
 class Solution
 {
+    /**
+     * @param Integer[] $nums
+     * @return NULL
+     */
+    function nextPermutation(array &$nums): void
+    {
+        $count = count($nums);
+        if ($count < 2) {
+            return;
+        }
+        $i = $count - 2;
+        while ($i >= 0 && $nums[$i] >= $nums[$i + 1]) {
+            $i--;
+        }
+        if ($i >= 0) {
+            $j = $count - 1;
+            while ($nums[$j] <= $nums[$i]) {
+                $j--;
+            }
+            [$nums[$i], $nums[$j]] = [$nums[$j], $nums[$i]];
+        }
+        $left = $i + 1;
+        $right = $count - 1;
+        while ($left < $right) {
+            [$nums[$left], $nums[$right]] = [$nums[$right], $nums[$left]];
+            $left++;
+            $right--;
+        }
+    }
 }
