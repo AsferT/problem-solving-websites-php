4f1d2166b02d99929983cbe8b6a22bdda22bfddb
“Asfer <asfer1@hotmail.com>
2025-12-11 12:23:55 +0000

    Added another Solution

--- src/Leetcode/Group0001_1000/Solution0032_longest_valid_parentheses/Solution.php
+++ src/Leetcode/Group0001_1000/Solution0032_longest_valid_parentheses/Solution.php
@@ -8,4 +8,30 @@ namespace Leetcode\Group0001_1000\Solution0032_longest_valid_parentheses;
  */
 class Solution
 {
+    /**
+     * @param String $s
+     * @return Integer
+     */
+    function longestValidParentheses(string $s): int
+    {
+        $maxResult = 0;
+        $stack = [-1];
+        $count = strlen($s);
+        if ($count < 2) {
+            return $maxResult;
+        }
+        for ($i = 0; $i < $count; $i++) {
+            if ($s[$i] === '(') {
+                $stack[] = $i;
+            } else {
+                array_pop($stack);
+                if ($stack) {
+                    $maxResult = max($maxResult, ($i - end($stack)));
+                } else {
+                    $stack[] = $i;
+                }
+            }
+        }
+        return $maxResult;
+    }
 }
