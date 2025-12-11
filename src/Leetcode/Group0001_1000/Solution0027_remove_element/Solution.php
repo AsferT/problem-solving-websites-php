a5bfb66ac1d453886f537115545283b44441a4e2
“Asfer <asfer1@hotmail.com>
2025-12-11 02:10:48 +0000

    Added another Solution

--- src/Leetcode/Group0001_1000/Solution0027_remove_element/Solution.php
+++ src/Leetcode/Group0001_1000/Solution0027_remove_element/Solution.php
@@ -8,4 +8,14 @@ namespace Leetcode\Group0001_1000\Solution0027_remove_element;
  */
 class Solution
 {
+    /**
+     * @param Integer[] $nums
+     * @param Integer $val
+     * @return Integer
+     */
+    function removeElement(array &$nums, int $val): int
+    {
+        $nums = array_filter($nums, fn($n) => $n !== $val);
+        return count($nums);
+    }
 }
