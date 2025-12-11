827cebc7744d6cbc860355d6d5abe7632ed1bfc3
“Asfer <asfer1@hotmail.com>
2025-12-11 12:30:07 +0000

    Added another Solution

--- src/Leetcode/Group0001_1000/Solution0033_search_in_rotated_sorted_array/Solution.php
+++ src/Leetcode/Group0001_1000/Solution0033_search_in_rotated_sorted_array/Solution.php
@@ -8,4 +8,14 @@ namespace Leetcode\Group0001_1000\Solution0033_search_in_rotated_sorted_array;
  */
 class Solution
 {
+    /**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer
+     */
+    function search(array $nums, int $target): int
+    {
+        $result = array_search($target, $nums);
+        return $result === false ? -1 : $result;
+    }
 }
