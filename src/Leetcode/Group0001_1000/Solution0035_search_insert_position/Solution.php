5dcddc573f1930898978a39016170c9794f746c5
“Asfer <asfer1@hotmail.com>
2025-12-11 12:45:27 +0000

    Added another Solution

--- src/Leetcode/Group0001_1000/Solution0035_search_insert_position/Solution.php
+++ src/Leetcode/Group0001_1000/Solution0035_search_insert_position/Solution.php
@@ -8,4 +8,25 @@ namespace Leetcode\Group0001_1000\Solution0035_search_insert_position;
  */
 class Solution
 {
+    /**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer
+     */
+    function searchInsert(array $nums, int $target): int
+    {
+        $result = array_search($target, $nums);
+        if (false !== $result) {
+            return $result;
+        }
+        $count = count($nums);
+        if (end($nums) < $target) {
+            return $count;
+        }
+        for ($i = 0; $i < $count; $i++) {
+            if ($nums[$i] > $target) {
+                return $i;
+            }
+        }
+    }
 }
