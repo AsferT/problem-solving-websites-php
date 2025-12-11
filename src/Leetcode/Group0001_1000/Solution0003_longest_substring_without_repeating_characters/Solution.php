36191a6f8224321b5007a1c544c045c837187ce0
“Asfer <asfer1@hotmail.com>
2025-12-11 23:40:19 +0000

    Added another Solution

--- src/Leetcode/Group0001_1000/Solution0003_longest_substring_without_repeating_characters/Solution.php
+++ src/Leetcode/Group0001_1000/Solution0003_longest_substring_without_repeating_characters/Solution.php
@@ -8,4 +8,24 @@ namespace Leetcode\Group0001_1000\Solution0003_longest_substring_without_repeati
  */
 class Solution
 {
+    /**
+     * @param String $s
+     * @return Integer
+     */
+    function lengthOfLongestSubstring(string $s): int
+    {
+        $sLen = strlen($s);
+        $start = 0;
+        $length = 0;
+        for ($i = 0; $i < $sLen; $i++) {
+            $char = $s[$i];
+            if (isset($arr[$char]) && $arr[$char] >= $start) {
+                $start = $arr[$char] + 1;
+            } elseif($i - $start === $length) {
+                $length++;
+            }
+            $arr[$char] = $i;
+        }
+        return $length;
+    }
 }
