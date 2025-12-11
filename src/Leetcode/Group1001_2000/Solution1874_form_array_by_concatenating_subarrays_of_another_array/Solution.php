4839cc522faeecaee7a28d665ac106014559b8b4
“Asfer <asfer1@hotmail.com>
2025-12-11 23:33:56 +0000

    Added another Solution

--- src/Leetcode/Group1001_2000/Solution1874_form_array_by_concatenating_subarrays_of_another_array/Solution.php
+++ src/Leetcode/Group1001_2000/Solution1874_form_array_by_concatenating_subarrays_of_another_array/Solution.php
@@ -8,4 +8,20 @@ namespace Leetcode\Group1001_2000\Solution1874_form_array_by_concatenating_subar
  */
 class Solution
 {
+    /**
+     * @param Integer[] $nums1
+     * @param Integer[] $nums2
+     * @return Integer
+     */
+    function minProductSum(array $nums1, array $nums2): int
+    {
+        sort($nums1);
+        rsort($nums2);
+        $count = count($nums1);
+        $sum = 0;
+        for ($i = 0; $i < $count; $i++) {
+            $sum += $nums1[$i] * $nums2[$i];
+        }
+        return $sum;
+    }
 }
