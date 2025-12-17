ab9f7a0655650c0ce7b0d66f9a8be95552deecbe
“Asfer <asfer1@hotmail.com>
2025-12-17 23:04:05 +0000

    Added solution

--- src/Leetcode/Group0001_1000/Solution0070_climbing_stairs/Solution.php
+++ src/Leetcode/Group0001_1000/Solution0070_climbing_stairs/Solution.php
@@ -8,4 +8,23 @@ namespace Leetcode\Group0001_1000\Solution0070_climbing_stairs;
  */
 class Solution
 {
+    /**
+     * @param Integer $n
+     * @return Integer
+     */
+    function climbStairs($n)
+    {
+        // Sounds like the Fibonacci problem
+        if ($n <= 2) {
+            return $n;
+        }
+        $prev1 = 1;
+        $prev2 = 2;
+        for ($i = 3; $i <= $n; $i++) {
+            $current = $prev1 + $prev2;
+            $prev1 = $prev2;
+            $prev2 = $current;
+        }
+        return $prev2;
+    }
 }
